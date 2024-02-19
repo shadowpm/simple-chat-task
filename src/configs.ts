3d75ad4b3e470af6fa940b0fd5c1e480de7ab78a
Parastoo Moghaddam <parastoomoghaddam1997@gmail.com>
2024-02-19 14:50:11 +0100

    add environment variables

---
+++ src/configs.ts
@@ -0,0 +1,4 @@
+export const token = process.env.REACT_APP_TOKEN || "q436hC9jf808";
+export const messageLimit = process.env.REACT_APP_MESSAGE_LIMIT || 10;
+export const reloadMessagesInterval =
+  process.env.REACT_APP_RELOAD_MESSAGES_INTERVAL || 60000;
