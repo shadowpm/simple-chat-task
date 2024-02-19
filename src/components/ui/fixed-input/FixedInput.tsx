0059fb62538845772b297fbbd7dc379be58eabf5
Parastoo Moghaddam <parastoomoghaddam1997@gmail.com>
2024-02-19 15:04:21 +0100

    create ui component for the message text field

---
+++ src/components/ui/fixed-input/FixedInput.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent, FormEvent } from "react";
+import "./styles.css";
+
+interface Props {
+  enteredMessage: string;
+  setEnteredMessage: (value: ChangeEvent<HTMLInputElement>) => void;
+  onSend: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+const FixedInput: React.FC<Props> = ({
+  enteredMessage,
+  setEnteredMessage,
+  onSend,
+}) => {
+  return (
+    <div className="form-container">
+      <form onSubmit={onSend}>
+        <label className="visually-hidden">Message:</label>
+        <input
+          value={enteredMessage}
+          onChange={setEnteredMessage}
+          className="messsage-field"
+          placeholder="Message"
+        />
+        <input className="submit-button" type="submit" value="Send" />
+      </form>
+    </div>
+  );
+};
+
+export default FixedInput;
